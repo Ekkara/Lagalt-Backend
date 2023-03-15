33761476ef9c2eca2232d4e2ddb3f8bb2b6151ac
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-03-15 23:38:11 +0100

    Feat: KeyCloak

--- Lagalt-Backend/Program.cs
+++ Lagalt-Backend/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Lagalt_Backend.Models;
 using Lagalt_Backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,16 +22,50 @@ builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IProjectService, ProjectService>();
 
 // Add CORS, allowing different domains to make requests to our app, might not be needed later
-builder.Services.AddCors(cors =>
+builder.Services.AddCors(options =>
 {
-    cors.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+    options.AddPolicy(name: "AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
 });
-
 // Added to keep JSON serialization as our default serialization method
 builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options =>
     options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
+// Configure authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = "https://lemur-3.cloud-iam.com/auth/realms/projectlagaltfullstack",
+            ValidAudience = "account",
+            IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
+            {
+                var client = new HttpClient();
+                var keyuri = "https://lemur-3.cloud-iam.com/auth/realms/projectlagaltfullstack/protocol/openid-connect/certs";
+                var response = client.GetAsync(keyuri).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                var keys = new JsonWebKeySet(responseString);
+                return keys.Keys;
+            }
+        };
+    });
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Lagalt Network API",
+        Description = "Get information about users, projects, Skills and ProjectApplication",
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
@@ -39,12 +76,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll"); // Cors stay open, but needs to be authorized by keycloaktoken to access endpoints
+app.UseRouting();
 app.UseAuthorization();
 
 // Enabling CORS
-app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
+//app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+// Use authentication middleware
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
